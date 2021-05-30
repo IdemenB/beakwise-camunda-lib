354257484aff9344f42574e411297b9fab0046d8
IdemenB <baris.idemen@beakwise.com>
2021-05-30 21:18:13 +0300

    fixes in error.rs

--- src/errors/errors.rs
+++ src/errors/errors.rs
@@ -1,6 +1,5 @@
 use reqwest;
 use serde_json;
-use std::error::Error as StandardError;
 use thiserror::Error;
 
 use crate::ProcessVariablesMap;
@@ -15,20 +14,18 @@ pub enum CamundaClientError {
     Io(#[from] std::io::Error),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
+#[error("CamundaProcessError: Code: {:?} Error: {:?} Variables: {:?}", self.code, self.error, self.variables)]
 pub struct CamundaProcessError {
     pub code: String,
     pub error: String,
     pub variables: Option<ProcessVariablesMap>,
 }
 
-impl StandardError for CamundaProcessError {}
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
+#[error("CamundaProcessFailure: Error: {:?} Retries: {:?} Retry Timeout: {:?} ms", self.error, self.retries, self.retry_timeout)]
 pub struct CamundaProcessFailure {
     pub error: String,
     pub retries: i32,
     pub retry_timeout: i64,
 }
-
-impl StandardError for CamundaProcessFailure {}
